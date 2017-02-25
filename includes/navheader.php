b2289b4787969b996df7905a9afab648a4f8331b
asewumi <adwumiogunbiyi@gmail.com>
2017-02-25 05:26:13 +0100

    added about and contactus page

---
+++ includes/navheader.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SONA</title>
+	 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+
+</head>
+<body>
+<!-- navigation section -->
+<nav class="navbar navbar-default " role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="index.php">SONA</a>
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-list-toggle">Toggle navbar</button>
+		</div>
+		
+		<div class="collapse navbar-collapse" id="nav-list-toggle">
+			<ul class="nav navbar-nav">
+				<li><a href="about.php">About us</a></li>
+				<li><a href="contactus.php">Contact us</a></li>
+			</ul>
+			<div class="navbar-right">
+				<button type="button" class="btn btn-default navbar-btn" data-target="#login-modal" data-toggle="modal">
+					Login <i class="fa fa-lock"></i> 
+				</button>
+				<button type="button" class="btn btn-default navbar-btn" data-target="#signup-modal" data-toggle="modal">
+					 Signup
+				</button>
+			</div>
+		</div>
+	</div>
+</nav>
